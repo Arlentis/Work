12704167ac40a25b5113d306da40b3306fda8ad0
Arlentis <87531272+Arlentis@users.noreply.github.com>
2021-10-01 16:41:15 +0100

    Update CopyUser.ps1

--- CopyUser.ps1
+++ CopyUser.ps1
@@ -1,11 +1,8 @@
-##### CHANGES TO BE MADE
-# Automate the copying of mailboxes? -- not sure whether this is the best idea really.. 
-
-##### ONLY CHANGE THE FOLLOWING 4 LINES
-$CopyFrom1 = "HameedT"
-$FirstName1 = "Callum"
-$LastName1 = "Deleteme"
-$Username1 = "CallumD"
+##### ONLY CHANGE THE FOLLOWING 4 LINES + MAILBOX SECTION
+$CopyFrom1 = ""
+$FirstName1 = ""
+$LastName1 = ""
+$Username1 = ""
 $UserPassword = ConvertTo-SecureString -AsPlainText "H34l1x!12" -Force
 
 ##### GETS TEMPLATE INFORMATION FROM EXISTING USER - OU, SELECTED PROPERTIES AND DATE FOR DESCRIPTION
@@ -50,61 +47,84 @@ Enable-Mailbox -identity $Username1 -Alias $Username1 -Database 'Archive Databas
 Get-Mailbox -ResultSize Unlimited -ErrorAction SilentlyContinue | Get-MailboxPermission -User $CopyFrom1 -ErrorAction SilentlyContinue | ft Identity -AutoSize -Wrap
 
 ##### EXIT EMC SESSION
-Exit-PSSession
+#Exit-PSSession
 
 ##### IF YOU WANT TO ADD MAILBOX PERMISSIONS THEN FILL IN THE IDENTITY FIELD BELOW, UNCOMMENT THE LINES AND RUN FROM HERE DOWN (HIGHLIGHT BELOW AND PRESS F8)
 
 ##### CONNECT TO EMC
-$password = Get-Content "C:\Users\CLAdmin\Documents\cred.txt" | ConvertTo-SecureString
-$Credential = New-Object -TypeName System.Management.Automation.PSCredential -ArgumentList "HLX\CLAdmin",$Password
-$session = New-PSSession -ConfigurationName Microsoft.Exchange -ConnectionUri http://iris.hlx.int/powershell/ -Credential $Credential -Authentication Kerberos
-Import-PSSession $session -AllowClobber -Verbose
-
+#$password = Get-Content "C:\Users\CLAdmin\Documents\cred.txt" | ConvertTo-SecureString
+#$Credential = New-Object -TypeName System.Management.Automation.PSCredential -ArgumentList "HLX\CLAdmin",$Password
+#$session = New-PSSession -ConfigurationName Microsoft.Exchange -ConnectionUri http://iris.hlx.int/powershell/ -Credential $Credential -Authentication Kerberos
+#Import-PSSession $session -AllowClobber -Verbose
 
 ##### FILL IN IDENTITY FIELDS AS REQUIRED
 
-## --------------------------------------- FCO ---------------------------------------------------------------------------------- ##
-#Add-MailboxPermission -Identity Healthline -User $Username1 -AccessRights FullAccess -InheritanceType All
-#Add-MailboxPermission -Identity MFAT -User $Username1 -AccessRights FullAccess -InheritanceType All
-#Add-MailboxPermission -Identity CovidVaccineENZ -User $Username1 -AccessRights FullAccess -InheritanceType All
-#Add-MailboxPermission -Identity Covid19VaccinationEn -User $Username1 -AccessRights FullAccess -InheritanceType All
-#Add-MailboxPermission -Identity CovidVaccineEnquirie -User $Username1 -AccessRights FullAccess -InheritanceType All
-## --------------------------------------- GPN ---------------------------------------------------------------------------------- ##
-#Add-MailboxPermission -Identity HealthlineProviderIntelligence -User $Username1 -AccessRights FullAccess -InheritanceType All
-#Add-MailboxPermission -Identity HealixMedical -User $Username1 -AccessRights FullAccess -InheritanceType All
-#Add-MailboxPermission -Identity RepatExpenses -User $Username1 -AccessRights FullAccess -InheritanceType All
-## --------------------------------------- GSOC ---------------------------------------------------------------------------------- ##
-#Add-MailboxPermission -Identity GSOC -User $Username1 -AccessRights FullAccess -InheritanceType All
-#Add-MailboxPermission -Identity IMT -User $Username1 -AccessRights FullAccess -InheritanceType All
-## --------------------------------------- IPT ----------------------------------------------------------------------------------- ##
-#Add-MailboxPermission -Identity HINTAdmin -User $Username1 -AccessRights FullAccess -InheritanceType All
-## --------------------------------------- HINT ---------------------------------------------------------------------------------- ##
-#Add-MailboxPermission -Identity InternationalAssista -User $Username1 -AccessRights FullAccess -InheritanceType All
-#Add-MailboxPermission -Identity InternationalHealthc -User $Username1 -AccessRights FullAccess -InheritanceType All
-#Add-MailboxPermission -Identity MedicalAssessments -User $Username1 -AccessRights FullAccess -InheritanceType All
-## --------------------------------------- PROJECTS ------------------------------------------------------------------------------ ##
-#Add-MailboxPermission -Identity Implementation -User $Username1 -AccessRights FullAccess -InheritanceType All
-## --------------------------------------- TRAINING & QUALITY -------------------------------------------------------------------- ##
-#Add-MailboxPermission -Identity TrainingAndQualityHi -User $Username1 -AccessRights FullAccess -InheritanceType All
-## --------------------------------------- MARKETING ----------------------------------------------------------------------------- ##
-#Add-MailboxPermission -Identity Communications -User $Username1 -AccessRights FullAccess -InheritanceType All
-#Add-MailboxPermission -Identity Marketing -User $Username1 -AccessRights FullAccess -InheritanceType All
-#Add-MailboxPermission -Identity SalesAndEnquiries -User $Username1 -AccessRights FullAccess -InheritanceType All
-## --------------------------------------- HHS ----------------------------------------------------------------------------------- ##
-#Add-MailboxPermission -Identity AprilUK -User $Username1 -AccessRights FullAccess -InheritanceType All
-#Add-MailboxPermission -Identity EduHealth -User $Username1 -AccessRights FullAccess -InheritanceType All
-#Add-MailboxPermission -Identity GeneralFDA -User $Username1 -AccessRights FullAccess -InheritanceType All
-#Add-MailboxPermission -Identity GroupHealth -User $Username1 -AccessRights FullAccess -InheritanceType All
-#Add-MailboxPermission -Identity HHSBankDetails -User $Username1 -AccessRights FullAccess -InheritanceType All
-#Add-MailboxPermission -Identity HHSInvoices -User $Username1 -AccessRights FullAccess -InheritanceType All
-#Add-MailboxPermission -Identity Lorium -User $Username1 -AccessRights FullAccess -InheritanceType All
-#Add-MailboxPermission -Identity Managed -User $Username1 -AccessRights FullAccess -InheritanceType All
-#Add-MailboxPermission -Identity MayfairAssist -User $Username1 -AccessRights FullAccess -InheritanceType All
-#Add-MailboxPermission -Identity MEC -User $Username1 -AccessRights FullAccess -InheritanceType All
-#Add-MailboxPermission -Identity miabinsurance -User $Username1 -AccessRights FullAccess -InheritanceType All
-#Add-MailboxPermission -Identity Romif -User $Username1 -AccessRights FullAccess -InheritanceType All
-#Add-MailboxPermission -Identity Zurich -User $Username1 -AccessRights FullAccess -InheritanceType All
+#region FCO (Foreign Commonwealth Office)
+#$Mailboxes1 = "Healthline","MFAT","CovidVaccineENZ","Covid19VaccinationEn","CovidVaccineEnquirie"
+#ForEach ($Mailbox1 in $Mailboxes1) {Add-MailboxPermission -Identity "$Mailbox1" -User $Username1 -AccessRights FullAccess -InheritanceType All ; Set-Mailbox $Mailbox1 –Grantsendonbehalfto @{add="$Username1"}}
+#endregion
+
+#region HMS (Healix Medical Services)
+#$Mailboxes1 = "HealthlineProviderIntelligence","HealixMedical","RepatExpenses","MedicalAssessments"
+#ForEach ($Mailbox1 in $Mailboxes1) {Add-MailboxPermission -Identity "$Mailbox1" -User $Username1 -AccessRights FullAccess -InheritanceType All ; Set-Mailbox $Mailbox1 –Grantsendonbehalfto @{add="$Username1"}}
+#endregion
+
+#region GPN (Global Provider Network)
+#$Mailboxes1 = "Research","GlobalProviderNetwor","Network.Development","GlobalNetwork"
+#ForEach ($Mailbox1 in $Mailboxes1) {Add-MailboxPermission -Identity "$Mailbox1" -User $Username1 -AccessRights FullAccess -InheritanceType All ; Set-Mailbox $Mailbox1 –Grantsendonbehalfto @{add="$Username1"}}
+#endregion
+
+#region GSOC (Global Security Operations Center)
+#$Mailboxes1 = "GSOC","IMT","GSOCTraining"
+#ForEach ($Mailbox1 in $Mailboxes1) {Add-MailboxPermission -Identity "$Mailbox1" -User $Username1 -AccessRights FullAccess -InheritanceType All ; Set-Mailbox $Mailbox1 –Grantsendonbehalfto @{add="$Username1"}}
+#endregion
+
+#region Invoice Processing Team
+#$Mailboxes1 = "HINTAdmin"
+#ForEach ($Mailbox1 in $Mailboxes1) {Add-MailboxPermission -Identity "$Mailbox1" -User $Username1 -AccessRights FullAccess -InheritanceType All ; Set-Mailbox $Mailbox1 –Grantsendonbehalfto @{add="$Username1"}}
+#endregion
+
+#region HINT (Healix International)
+#$Mailboxes1 = "InternationalAssista","InternationalHealthc"
+#ForEach ($Mailbox1 in $Mailboxes1) {Add-MailboxPermission -Identity "$Mailbox1" -User $Username1 -AccessRights FullAccess -InheritanceType All ; Set-Mailbox $Mailbox1 –Grantsendonbehalfto @{add="$Username1"}}
+#endregion
+
+#region Projects
+#$Mailboxes1 = "Implementation"
+#ForEach ($Mailbox1 in $Mailboxes1) {Add-MailboxPermission -Identity "$Mailbox1" -User $Username1 -AccessRights FullAccess -InheritanceType All ; Set-Mailbox $Mailbox1 –Grantsendonbehalfto @{add="$Username1"}}
+#endregion
+
+#region T&Q (Training & Quality)
+#$Mailboxes1 = "TrainingAndQualityHi"
+#ForEach ($Mailbox1 in $Mailboxes1) {Add-MailboxPermission -Identity "$Mailbox1" -User $Username1 -AccessRights FullAccess -InheritanceType All ; Set-Mailbox $Mailbox1 –Grantsendonbehalfto @{add="$Username1"}}
+#endregion
+
+#region Marketing
+#$Mailboxes1 = "Communications","Marketing","SalesAndEnquiries"
+#ForEach ($Mailbox1 in $Mailboxes1) {Add-MailboxPermission -Identity "$Mailbox1" -User $Username1 -AccessRights FullAccess -InheritanceType All ; Set-Mailbox $Mailbox1 –Grantsendonbehalfto @{add="$Username1"}}
+#endregion
+
+#region Sales
+#$Mailboxes1 = "SalesSupport"
+#ForEach ($Mailbox1 in $Mailboxes1) {Add-MailboxPermission -Identity "$Mailbox1" -User $Username1 -AccessRights FullAccess -InheritanceType All ; Set-Mailbox $Mailbox1 –Grantsendonbehalfto @{add="$Username1"}}
+#endregion
+
+#region HHS (Healix Health Services, Marcus's team)
+#$Mailboxes1 = "GeneralFDA","HHSBankDetails","HHSInvoices","HISPMI","Managed","MayfairAssist","MEC","Romif","PrioryGroup","techmahindra"
+#ForEach ($Mailbox1 in $Mailboxes1) {Add-MailboxPermission -Identity "$Mailbox1" -User $Username1 -AccessRights FullAccess -InheritanceType All ; Set-Mailbox $Mailbox1 –Grantsendonbehalfto @{add="$Username1"}}
+#endregion
+
+#region HHS (Healix Health Services)
+#$Mailboxes1 = "AprilUK","EduHealth","GeneralFDA","GroupHealth","HHSBankDetails","HHSInvoices","Lorium","Managed","MayfairAssist","MEC","MiabInsurance","Romif","Zurich","HISPMI","PrioryGroup","ShiftLeader","AprilMembership","CignPost","ClinicalQueries"
+#ForEach ($Mailbox1 in $Mailboxes1) {Add-MailboxPermission -Identity "$Mailbox1" -User $Username1 -AccessRights FullAccess -InheritanceType All ; Set-Mailbox $Mailbox1 –Grantsendonbehalfto @{add="$Username1"}}
+#endregion
+
+#region HHS 2 (Healix Health Services, David Butler's team)
+#$Mailboxes1 = "Aetna","Chevron","CSHealthcare","GeneralFDA","Henner","HHSInvoices","Managed","MayfairAssist","MEC","MSHINT","Network.Development","RedArc","Techmahindra","William-Russell","Zurich"
+#ForEach ($Mailbox1 in $Mailboxes1) {Add-MailboxPermission -Identity "$Mailbox1" -User $Username1 -AccessRights FullAccess -InheritanceType All ; Set-Mailbox $Mailbox1 -Grantsendonbehalfto @{add="$Username1"}}
+#endregion
 
 ##### EXIT EMC SESSION
+Remove-PSSession $session -ErrorAction SilentlyContinue
 Exit-PSSession
 
